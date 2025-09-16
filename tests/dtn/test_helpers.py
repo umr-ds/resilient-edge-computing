f71cb7447fdb0449ff260eab6fb15e01d8bef1c0
Markus Sommer <msommer@informatik.uni-marburg.de>
2025-09-16 19:51:33 +0200

    Reduce alphabet for eid generation

--- tests/dtn/test_helpers.py
+++ tests/dtn/test_helpers.py
@@ -10,8 +10,33 @@ from rec.dtn.eid import EID
 
 @st.composite
 def dtn_eid(draw: st.DrawFn, singleton=True) -> EID:
-    node: str = draw(st.text())
-    service: str = draw(st.text())
+    node: str = draw(
+        st.text(
+            alphabet=st.characters(
+                codec="ascii",
+                categories=["L", "N"],
+                include_characters=[
+                    "-",
+                    ".",
+                    "_",
+                    "~",
+                    "!",
+                    "$",
+                    "&",
+                    "'",
+                    "(",
+                    ")",
+                    "*",
+                    "+",
+                    ",",
+                    ";",
+                    "=",
+                ],
+            ),
+            min_size=1,
+        )
+    )
+    service: str = draw(st.text(alphabet=st.characters(codec="ascii")))
     if singleton:
         eid = EID.dtn(node=node, service=service)
     else:
